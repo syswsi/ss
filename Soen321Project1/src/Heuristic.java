48c89f10f9f0ae4acf8b6b250d4adc42478af5ed
j-rani <jayanti_rani@hotmail.fr>
2014-10-30 12:19:28 -0400

    1st push

---
+++ Soen321Project1/src/Heuristic.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Heuristic {
+
+	HashMap<String, Double> hm = new HashMap<String, Double>();
+	
+	public void putCharacters (){
+		hm.put("a", new Double(8.167));
+		hm.put("b", new Double(8.167));
+		hm.put("c", new Double(8.167));
+		hm.put("d", new Double(8.167));
+		hm.put("e", new Double(8.167));
+		hm.put("f", new Double(8.167));
+		hm.put("g", new Double(8.167));
+		hm.put("h", new Double(8.167));
+		hm.put("i", new Double(8.167));
+		hm.put("j", new Double(8.167));
+		hm.put("k", new Double(8.167));
+		hm.put("l", new Double(8.167));
+		hm.put("m", new Double(8.167));
+		hm.put("n", new Double(8.167));
+		hm.put("o", new Double(8.167));
+		hm.put("p", new Double(8.167));
+		hm.put("q", new Double(8.167));
+		hm.put("r", new Double(8.167));
+		hm.put("s", new Double(8.167));
+		hm.put("t", new Double(8.167));
+		hm.put("u", new Double(8.167));
+		hm.put("v", new Double(8.167));
+		hm.put("w", new Double(8.167));
+		hm.put("x", new Double(8.167));
+		hm.put("y", new Double(8.167));
+		hm.put("z", new Double(8.167));
+	}
+}
