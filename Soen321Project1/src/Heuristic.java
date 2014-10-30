775fadc6c29e031b3b233f45df5524eef7e9bc5f
j-rani <jayanti_rani@hotmail.fr>
2014-10-30 12:29:18 -0400

    value changes

--- Soen321Project1/src/Heuristic.java
+++ Soen321Project1/src/Heuristic.java
@@ -6,30 +6,32 @@ public class Heuristic {
 	
 	public void putCharacters (){
 		hm.put("a", new Double(8.167));
-		hm.put("b", new Double(8.167));
-		hm.put("c", new Double(8.167));
-		hm.put("d", new Double(8.167));
-		hm.put("e", new Double(8.167));
-		hm.put("f", new Double(8.167));
-		hm.put("g", new Double(8.167));
-		hm.put("h", new Double(8.167));
-		hm.put("i", new Double(8.167));
-		hm.put("j", new Double(8.167));
-		hm.put("k", new Double(8.167));
-		hm.put("l", new Double(8.167));
-		hm.put("m", new Double(8.167));
-		hm.put("n", new Double(8.167));
-		hm.put("o", new Double(8.167));
-		hm.put("p", new Double(8.167));
-		hm.put("q", new Double(8.167));
-		hm.put("r", new Double(8.167));
-		hm.put("s", new Double(8.167));
-		hm.put("t", new Double(8.167));
-		hm.put("u", new Double(8.167));
-		hm.put("v", new Double(8.167));
-		hm.put("w", new Double(8.167));
-		hm.put("x", new Double(8.167));
-		hm.put("y", new Double(8.167));
-		hm.put("z", new Double(8.167));
+		hm.put("b", new Double(1.492));
+		hm.put("c", new Double(2.782));
+		hm.put("d", new Double(4.253));
+		hm.put("e", new Double(12.702));
+		hm.put("f", new Double(2.228));
+		hm.put("g", new Double(2.015));
+		hm.put("h", new Double(6.094));
+		hm.put("i", new Double(6.966));
+		hm.put("j", new Double(0.153));
+		hm.put("k", new Double(0.772));
+		hm.put("l", new Double(4.025));
+		hm.put("m", new Double(2.406));
+		hm.put("n", new Double(6.749));
+		hm.put("o", new Double(7.507));
+		hm.put("p", new Double(1.929));
+		hm.put("q", new Double(0.095));
+		hm.put("r", new Double(5.987));
+		hm.put("s", new Double(6.327));
+		hm.put("t", new Double(9.056));
+		hm.put("u", new Double(2.758));
+		hm.put("v", new Double(0.978));
+		hm.put("w", new Double(2.360));
+		hm.put("x", new Double(0.150));
+		hm.put("y", new Double(1.974));
+		hm.put("z", new Double(0.074));
 	}
+	
+	
 }
